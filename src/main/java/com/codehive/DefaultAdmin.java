2b514131552374adfead96505cda6808f7f67a17
MOHAMED ACHAROUAOU <acharouaoumohamed@gmail.com>
2025-03-18 00:01:32 +0000

    feat: add profile annotation to DefaultAdmin for non-test environments

--- src/main/java/com/codehive/DefaultAdmin.java
+++ src/main/java/com/codehive/DefaultAdmin.java
@@ -9,12 +9,14 @@ import com.codehive.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Profile("!test")
 @Component
 @RequiredArgsConstructor
 public class DefaultAdmin implements CommandLineRunner {

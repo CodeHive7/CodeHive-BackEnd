7c922c9cc7cbe36d895158cc9993149ef37fd7e3
MOHAMED ACHAROUAOU <acharouaoumohamed@gmail.com>
2025-03-22 22:11:55 +0000

    test: update CodehiveApplicationTests for in-memory database configuration

--- src/test/java/com/codehive/CodehiveApplicationTests.java
+++ src/test/java/com/codehive/CodehiveApplicationTests.java
@@ -3,13 +3,21 @@ package com.codehive;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.main.allow-bean-definition-overriding=true",
+    "spring.jpa.hibernate.ddl-auto=none",
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "app.init.enabled=false"  // Disable your DefaultAdmin initializer
+})
 @ActiveProfiles("test")
 class CodehiveApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Just tests the spring context loads
+    }
 }

67ef51b4d9ce29c8670f538828fcbc9436cdff8a
MOHAMED ACHAROUAOU <acharouaoumohamed@gmail.com>
2025-03-23 23:11:52 +0000

    feat: add transactional support to updateUserProfile and getUserProfile methods in UserServiceImpl

--- src/main/java/com/codehive/service/impl/UserServiceImpl.java
+++ src/main/java/com/codehive/service/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import com.codehive.service.UserService;
 import com.codehive.vm.UserVM;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -17,6 +18,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
+    @Transactional
     public UserDto updateUserProfile(String username, UserVM vm) {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new RuntimeException("User not found"));
@@ -26,6 +28,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public UserDto getUserProfile(String username) {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new RuntimeException("User not found"));

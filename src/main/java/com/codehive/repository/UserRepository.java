c1380ffaa24451e5e4bb1e63de9773681d5d6ce0
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2025-04-19 18:19:28 +0100

    feat: add method to find user by email with roles and permissions

--- src/main/java/com/codehive/repository/UserRepository.java
+++ src/main/java/com/codehive/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 
 import java.util.Optional;
 
@@ -21,6 +22,13 @@ public interface UserRepository extends JpaRepository<User,Long> {
           WHERE u.username = :username
 """)
     Optional<User> findByUsernameWithRolesAndPermissions(@Param("username") String username);
-
-    boolean existsByEmail(String email);
+    @Query("""
+          SELECT DISTINCT u 
+          FROM users u
+          LEFT JOIN FETCH u.roles r
+          LEFT JOIN FETCH r.permissions 
+          WHERE u.email = :email
+""")
+    Optional<User> findByEmailWithRolesAndPermissions(String email);
+    Optional existsByEmail(@Param("email") String email);
 }
